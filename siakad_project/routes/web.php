32a47a1f8e969fb290fc582eac5e6b521efbd6de
antytii <ityaaa04@gmail.com>
2025-11-13 19:46:31 +0800

    bagian dashboard mahasiswa belum rampung

--- siakad_project/routes/web.php
+++ siakad_project/routes/web.php
@@ -11,14 +11,12 @@ Route::get('/siakad-login-test', function () {
     return view('pages.auth.login');
 });
 
-// Route untuk pengujian tampilan Dashboard Mahasiswa
+// Route buat uji tampilan Dashboard Mahasiswa
 Route::get('/dashboard-mhs', function () {
-    // Menampilkan file Blade Dashboard yang baru kita buat
     return view('pages.dashboard.index');
 });
 
-// Route untuk pengujian tampilan Halaman Jadwal Kuliah Mahasiswa
+// Route buat uji tampilan Jadwal Kuliah Mahasiswa
 Route::get('/jadwal-mhs', function () {
-    // Menampilkan file Blade Jadwal yang baru kita buat
     return view('pages.mahasiswa.jadwal');
 });
